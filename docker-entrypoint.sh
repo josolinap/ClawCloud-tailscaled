a2dd333d93cb88704d104b8cbf567366fcc7f1b4
Jo <147356717+josolinap@users.noreply.github.com>
2025-09-02 22:30:18 +0800

    Update docker-entrypoint.sh
    1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# Allow all incoming traffic
-iptables -P INPUT ACCEPT
-
 unset KUBERNETES_SERVICE_HOST KUBERNETES_PORT KUBERNETES_PORT_443_TCP
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/services.conf
