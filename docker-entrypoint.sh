c6f62ba1696471945e351935fc5e1caec8e3d269
Jo <147356717+josolinap@users.noreply.github.com>
2025-09-03 00:13:57 +0800

    Update docker-entrypoint.sh
    g

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+if command -v iptables >/dev/null 2>&1; then
 # Flush old iptables rules
 iptables -F
 iptables -X
@@ -8,7 +9,7 @@ iptables -X
 # Allow only port 8080 incoming
 iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 iptables -A INPUT -p tcp --syn -j DROP
-
+fi
 # Unset K8s env variables if running in Kubernetes
 unset KUBERNETES_SERVICE_HOST KUBERNETES_PORT KUBERNETES_PORT_443_TCP
 
