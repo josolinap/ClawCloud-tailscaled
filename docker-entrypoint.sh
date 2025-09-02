17c3664ba50d5272c305681222271d586eac5be1
Jo <147356717+josolinap@users.noreply.github.com>
2025-09-02 22:36:22 +0800

    Update docker-entrypoint.sh
    1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6 
+
 unset KUBERNETES_SERVICE_HOST KUBERNETES_PORT KUBERNETES_PORT_443_TCP
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/services.conf
