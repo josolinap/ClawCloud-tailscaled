bd3ec714312b65702e2fc8b4bb9b6b6536e6864c
Jo <147356717+josolinap@users.noreply.github.com>
2025-09-02 23:58:57 +0800

    Update docker-entrypoint.sh
    1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Allow HTTP server port
+iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+
+# Optional: avoid dropping Tailscale traffic in ephemeral/cloud environments
+# iptables -A INPUT -p tcp --syn -j DROP   # Commented to prevent blocking Tailscale
+
 unset KUBERNETES_SERVICE_HOST KUBERNETES_PORT KUBERNETES_PORT_443_TCP
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/services.conf
