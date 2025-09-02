9176d8a6b3aeb395753ef7e5ec9dfc58276d3cd1
Jo <147356717+josolinap@users.noreply.github.com>
2025-09-03 00:05:12 +0800

    Update docker-entrypoint.sh
    1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+set -e
 
-# Allow HTTP server port
+# Flush old iptables rules
+iptables -F
+iptables -X
+
+# Allow only port 8080 incoming
 iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+iptables -A INPUT -p tcp --syn -j DROP
 
-# Optional: avoid dropping Tailscale traffic in ephemeral/cloud environments
-# iptables -A INPUT -p tcp --syn -j DROP   # Commented to prevent blocking Tailscale
-
+# Unset K8s env variables if running in Kubernetes
 unset KUBERNETES_SERVICE_HOST KUBERNETES_PORT KUBERNETES_PORT_443_TCP
+
+# Start supervisord
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/services.conf
